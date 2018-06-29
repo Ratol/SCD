d189134e29263a8a8087dbef15ef102760d11472
Ratol <1136981032@qq.com>
2018-06-29 20:41:13 +0800

    update

--- code/SCD/src/main/java/com/sinotrans/RatolApplication.java
+++ code/SCD/src/main/java/com/sinotrans/RatolApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RatolApplication {
+public class SDCApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RatolApplication.class, args);
